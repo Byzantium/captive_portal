1fcff961104972a2e32c492328bd1e76264611d9
haxwithaxe <me@haxwithaxe.net>
2013-11-03 15:56:51 -0500

    breaking up the script into smaller chuncks

--- captive-portal.sh
+++ captive-portal.sh
@@ -11,6 +11,69 @@
 IPTABLES=/usr/sbin/iptables
 ARP=/sbin/arp
 
+
+exempt_nonclient_traffic() {
+ # Exempt traffic which does not originate from the client network.
+         $IPTABLES -t mangle -A PREROUTING ! -s $CLIENTNET -j RETURN
+}
+
+grab_new_clients() {
+	# Traffic not exempted by the previous rules gets kicked to the captive
+	# portal chain.  When a user clicks through a rule is inserted before
+	# this one that matches them with a RETURN.
+	$IPTABLES -t mangle -A PREROUTING -j internet
+}
+
+mark_new_clients() {
+	# Traffic not coming from an accepted user gets marked 99.
+	$IPTABLES -t mangle -A internet -j MARK --set-mark 99
+}
+
+iptables_redir_marked() {
+	rule=PREROUTING
+	proto=$2
+	port=$3
+	clientip=$4
+	dport=$5
+	$IPTABLES -t nat -A $rule -m mark --mark 99 -p $proto --dport $port
+	-j DNAT --to-destination $clientip:$dport
+}
+
+iptables_unredir_marked() {
+	rule=POSTROUTING
+    proto=$2
+    port=$3
+    client_net=$4
+    sport=$5
+    $IPTABLES -t nat -A $rule -d $client_net -p $proto --sport $port \
+            -j SNAT --to-source :$sport
+}
+
+redirect_marked_to_portal() {
+	# Traffic which has been marked 99 and is headed for 80/TCP or 443/TCP
+    # should be redirected to the captive portal web server.
+    iptables_redir_marked tcp 80 $CLIENTIP 31337
+    iptables_redir_marked tcp 443 $CLIENTIP 31338
+    iptables_redir_marked udp 53 $CLIENTIP 31339
+}
+
+redirect_marked_from_portal() {
+	# HTTP replies come from the same port the requests were received by.
+    # Rewrite the outbound packets to appear to come from the appropriate
+    # ports.
+    iptables_unredir_marked tcp 31337 $CLIENTNET 80
+    iptables_unredir_marked tcp 31338 $CLIENTNET 443
+    # Replies from fake_dns.py come from the same port because they're
+    # UDP.  Rewrite the packet headers so it loos like it's from port
+    # 53/udp.
+    iptables_unredir_marked udp 31339 $CLIENTNET 53
+}
+
+drop_nonexempt_marked() {
+    # All other traffic which is marked 99 is just dropped
+	$IPTABLES -t filter -A FORWARD -m mark --mark 99 -j DROP
+}
+
 # Set up the choice tree of options that can be passed to this script.
 case "$1" in
     'initialize')
@@ -24,43 +87,31 @@ case "$1" in
         CLIENTNET=`echo $2 | sed 's/1$/0\/24/'`
 
         # Exempt traffic which does not originate from the client network.
-        $IPTABLES -t mangle -A PREROUTING -p tcp ! -s $CLIENTNET -j RETURN
-        $IPTABLES -t mangle -A PREROUTING -p udp ! -s $CLIENTNET -j RETURN
+		exempt_nonclient_traffic        
 
         # Traffic not exempted by the above rules gets kicked to the captive
         # portal chain.  When a use clicks through a rule is inserted above
         # this one that matches them with a RETURN.
-        $IPTABLES -t mangle -A PREROUTING -j internet
+		grab_new_clients    
 
         # Traffic not coming from an accepted user gets marked 99.
-        $IPTABLES -t mangle -A internet -j MARK --set-mark 99
+        mark_new_clients
 
         # $2 is actually the IP address of the client interface, so let's make
         # it a bit more clear.
         CLIENTIP=$2
 
+
         # Traffic which has been marked 99 and is headed for 80/TCP or 443/TCP
         # should be redirected to the captive portal web server.
-        $IPTABLES -t nat -A PREROUTING -m mark --mark 99 -p tcp --dport 80 \
-            -j DNAT --to-destination $CLIENTIP:31337
-        $IPTABLES -t nat -A PREROUTING -m mark --mark 99 -p tcp --dport 443 \
-            -j DNAT --to-destination $CLIENTIP:31338
-        $IPTABLES -t nat -A PREROUTING -m mark --mark 99 -p udp --dport 53 \
-            -j DNAT --to-destination $CLIENTIP:31339
+		redirect_marked_to_portal
 
         # HTTP replies come from the same port the requests were received by.
         # Rewrite the outbound packets to appear to come from the appropriate
-        # ports.
-        $IPTABLES -t nat -A POSTROUTING -d $CLIENTNET -p tcp --sport 31337 \
-            -j SNAT --to-source :80
-        $IPTABLES -t nat -A POSTROUTING -d $CLIENTNET -p tcp --sport 31338 \
-            -j SNAT --to-source :443
-
-        # Replies from fake_dns.py come from the same port because they're
-        # UDP.  Rewrite the packet headers so it loos like it's from port
+        # ports. Also replies from fake_dns.py come from the same port because
+		# they're UDP.  Rewrite the packet headers so it loos like it's from port
         # 53/udp.
-        $IPTABLES -t nat -A POSTROUTING -d $CLIENTNET -p udp --sport 31339 \
-            -j SNAT --to-source :53
+        redirect_marked_from_portal
 
         # All other traffic which is marked 99 is just dropped
         $IPTABLES -t filter -A FORWARD -m mark --mark 99 -j DROP
@@ -84,39 +135,35 @@ case "$1" in
         $IPTABLES -t filter -A INPUT -p udp --dport 5353 -j ACCEPT
         $IPTABLES -t filter -A INPUT -p udp --dport 31339 -j ACCEPT
         $IPTABLES -t filter -A INPUT -p icmp -j ACCEPT
-
         # But reject anything else coming from unrecognized users.
         $IPTABLES -t filter -A INPUT -m mark --mark 99 -j DROP
-
         exit 0
         ;;
+
     'add')
         # $2: IP address of client.
         CLIENT=$2
-
         # Isolate the MAC address of the client in question.
         CLIENTMAC=`$ARP -n | grep ':' | grep $CLIENT | awk '{print $3}'`
-
         # Add the MAC address of the client to the whitelist, so it'll be able
         # to access the mesh even if its IP address changes.
         $IPTABLES -t mangle -I internet -m mac --mac-source \
             $CLIENTMAC -j RETURN
-
         exit 0
         ;;
+
     'remove')
         # $2: IP address of client.
         CLIENT=$2
-
         # Isolate the MAC address of the client in question.
         CLIENTMAC=`$ARP -n | grep ':' | grep $CLIENT | awk '{print $3}'`
 
         # Delete the MAC address of the client from the whitelist.
         $IPTABLES -t mangle -D internet -m mac --mac-source \
             $CLIENTMAC -j RETURN
-
         exit 0
         ;;
+
     'purge')
         # Purge all of the IP tables rules.
         $IPTABLES -F
@@ -127,19 +174,21 @@ case "$1" in
         $IPTABLES -t mangle -X
         $IPTABLES -t filter -F
         $IPTABLES -t filter -X
-
         exit 0
         ;;
+
     'list')
         # Display the currently running IP tables ruleset.
         $IPTABLES --list -n
         $IPTABLES --list -t nat -n
         $IPTABLES --list -t mangle -n
         $IPTABLES --list -t filter -n
-
         exit 0
         ;;
+
     *)
         echo "USAGE: $0 {initialize <IP> <interface>|add <IP> <interface>|remove <IP> <interface>|purge|list}"
         exit 0
-    esac
+		;;
+
+esac
